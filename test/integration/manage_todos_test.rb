fef429b059119a4cfa6ad7fa7dd6e50fab358b44
Patrik Bóna <patrik.bona@mrhead.sk>
2015-10-02 10:17:29 +0200

    Fix test file name typo

--- test/integration/manage_todos_test.rb
+++ test/integration/manage_todos_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-class ManageTotosTest < ActionDispatch::IntegrationTest
+class ManageTodosTest < ActionDispatch::IntegrationTest
   def setup
     Capybara.current_driver = :webkit
   end
