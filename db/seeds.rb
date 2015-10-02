9fbdf742c04f5def1392388726c75230cc350086
Patrik Bóna <patrik.bona@mrhead.sk>
2015-10-02 13:02:31 +0200

    Update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,7 @@
 TodoList.destroy_all
 
-TodoList.create!(name: "My awesome Todo list!")
+todo_list = TodoList.create!(name: "My awesome Todo list!")
+
+todo_list.todos.create!(name: "I am first!")
+todo_list.todos.create!(name: "Complete me, please!")
+todo_list.todos.create!(name: "Haha, done!", completed: true)
