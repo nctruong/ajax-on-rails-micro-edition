2c2f5ffd78c0a0cbbf45804951a288b48d0d9707
Patrik Bóna <patrik.bona@mrhead.sk>
2015-10-03 10:06:17 +0200

    Update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,4 +4,7 @@ todo_list = TodoList.create!(name: "My awesome Todo list!")
 
 todo_list.todos.create!(name: "I am first!")
 todo_list.todos.create!(name: "Complete me, please!")
+todo_list.todos.create!(name: "Better be third, than forth")
+todo_list.todos.create!(name: ":(")
 todo_list.todos.create!(name: "Haha, done!", completed: true)
+todo_list.todos.create!(name: "Me too!", completed: true)
