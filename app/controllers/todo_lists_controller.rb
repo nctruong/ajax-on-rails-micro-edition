0ab746eb4381507225f3f37c700ffcfb8fe7c235
Patrik Bóna <patrik.bona@mrhead.sk>
2015-10-03 14:14:29 +0200

    Refactor TodoListsController
    Use `id` for finding a todo list which we want to show.

--- app/controllers/todo_lists_controller.rb
+++ app/controllers/todo_lists_controller.rb
@@ -1,6 +1,10 @@
 class TodoListsController < ApplicationController
+  def index
+    redirect_to TodoList.first
+  end
+
   def show
-    @todo_list = TodoList.first
+    @todo_list = TodoList.find(params[:id])
     @todo = Todo.new
   end
 end
--- test/controllers/todo_lists_controller_test.rb
+++ test/controllers/todo_lists_controller_test.rb
@@ -1,9 +1,17 @@
 require 'test_helper'
 
 class TodoListsControllerTest < ActionController::TestCase
-  test "should get show" do
-    get :show
-    assert_response :success
+  test "index redirects to the first todo_list" do
+    get :index
+
+    assert_redirected_to todo_list_path(TodoList.first)
   end
 
+  test "should get show" do
+    todo_list = todo_lists(:todo_list)
+
+    get :show, id: todo_list.id
+
+    assert_select "h1", todo_list.name
+  end
 end
